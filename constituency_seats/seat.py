543e9f94312b634e9e762d0143e0f03c73fc3215
JoshMcDonagh <jmmarland99@gmail.com>
2024-05-25 23:55:12 +0100

    Added vote share and seat share data

--- constituency_seats/seat.py
+++
@@ -1,8 +0,0 @@
-class Seat:
-    def __init__(self, constituency_id, prev_election_vote_shares):
-        self.constituency_id = constituency_id
-        self.prev_election_vote_shares = prev_election_vote_shares
-        self.prev_party_winner = max(self.prev_election_vote_shares, key=self.prev_election_vote_shares.get)
-
-
-
--- political_parties/party.py
+++
@@ -1,6 +0,0 @@
-class Party:
-    def __init__(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
